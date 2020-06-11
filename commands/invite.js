9d49fee5b6adb76471248b72389c575be9c305c7
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-11 15:42:17 -0400

    invite command

---
+++ commands/invite.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new MessageEmbed()
+    .setTitle("GiveawayBot's Invite Link")
+    .setDescription('Invite GiveawayBot with this [link](https://discord.com/oauth2/authorize?client_id=720374601649094656&permissions=8&scope=bot)!')
+    .setColor('BLUE');
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "invite",
+    description: "Sends GiveawayBot's invite link.",
+    usage: "g!invite",
+    accessableby: "Everyone",
+    aliases: []
+}
