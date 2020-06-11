b20110eec289e4cac5e12a1d54bd88f98c3cce05
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-11 12:15:08 -0400

    test 8

--- commands/list.js
+++ commands/list.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     let activeGiveaways = bot.giveawaysManager.giveaways.filter((g) => g.guildID === message.guild.id);
 
-    message.channel.send(`${activeGiveaways.map((g) => g.messageID.join(', '))}`);
+    message.channel.send(`${activeGiveaways.map((g) => g.messageID).join(', '))}`);
 }
 
 module.exports.config = {
