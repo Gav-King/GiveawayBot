49704f3186d844c45df0980f129792e02b84a20e
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-06-11 10:32:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,46 @@ const db = require('quick.db');
 
 if(!db.get("giveaways")) db.set("giveaways", []);
 
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if(jsfile.length <= 0) {
+         return console.log("Couldn't Find Commands");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name);
+        });
+    });
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+    if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) {
+	return message.channel.send("Say \`g!help\` for a list of commands!");
+    }
+
+    let prefix = 'g!';
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
+    if(commandfile) commandfile.run(bot,message,args);
+});
+
+require("./util/eventHandler")(bot);
+
 const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
  
     // This function is called when the manager needs to get all the giveaway stored in the database.
@@ -49,47 +89,7 @@ const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
  
 };
 
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-    if(err) console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0) {
-         return console.log("Couldn't Find Commands");
-    }
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name);
-        });
-    });
-});
-
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
-    if (message.content.match(new RegExp(`^<@!?${bot.user.id}>( |)$`))) {
-	return message.channel.send("Say \`g!help\` for a list of commands!");
-    }
-
-    let prefix = 'g!';
-
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)));
-    if(commandfile) commandfile.run(bot,message,args);
-});
-
-require("./util/eventHandler")(bot);
-
-const manager = new GiveawaysManagerWithOwnDatabase(bot, {
+const manager = new GiveawayManagerWithOwnDatabase(bot, {
   storage: false,
   updateCountdownEvery: 10000,
   default: {
