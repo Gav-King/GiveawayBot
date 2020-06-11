5654fe4cc6233fd8f06c936b78727e167831a49e
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-11 11:47:00 -0400

    new command

---
+++ commands/list.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    let activeGiveaways = bot.giveawaysManager.giveaways.filter(x => x.guildID === message.guild.id && !x.ended);
+
+    message.channel.send(`${activeGiveaways.join('\n')}`);
+}
+
+module.exports.config = {
+    name: "list",
+    description: "Lists all the active giveaways in the server.",
+    usage: "g!list",
+    accessableby: "Everyone",
+    aliases: []
+}
