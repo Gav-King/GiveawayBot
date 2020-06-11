0832f1888bfd58968fde768b408cc9680ac28cf8
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-11 12:55:34 -0400

    test 18

--- commands/list.js
+++ commands/list.js
@@ -4,11 +4,11 @@ module.exports.run = async (bot, message, args) => {
     let activeGiveaways = bot.giveawaysManager.giveaways.filter((g) => g.guildID === message.guild.id);
     let giveaways = activeGiveaways.filter((g) => !g.ended);
 
-    if (!activeGiveaways) {
-      return message.channel.send("There are no active giveaways in this server.");
-    }
-
+    try {
     message.channel.send(`${giveaways.map((g) => `**Prize**: ${g.data.prize} **Hosted By**: ${g.hostedBy.username}\n\n**ID**: ${g.messageID}`).join("\n\n")}`);
+  } catch(err) {
+    message.channel.send('There are currently no active giveaways in this server.')
+  }
 }
 
 module.exports.config = {
