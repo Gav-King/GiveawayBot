cab3a172cf02e3484703a61b8cef6a95af5ff2d0
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-06-11 10:14:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,50 @@ const bot = new Discord.Client();
 const { GiveawaysManager } = require('discord-giveaways');
 const fs = require('fs');
 const config = require('./config.json');
+const db = require('quick.db');
+
+if(!db.get("giveaways")) db.set("giveaways", []);
+
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+ 
+    // This function is called when the manager needs to get all the giveaway stored in the database.
+    async getAllGiveaways(){
+        // Get all the giveaway in the database
+        return db.get("giveaways");
+    }
+ 
+    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+    async saveGiveaway(messageID, giveawayData){
+        // Add the new one
+        db.push("giveaways", giveawayData);
+        // Don't forget to return something!
+        return true;
+    }
+ 
+    async editGiveaway(messageID, giveawayData){
+        // Gets all the current giveaways
+        const giveaways = db.get("giveaways");
+        // Remove the old giveaway from the current giveaways ID
+        const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
+        // Push the new giveaway to the array
+        newGiveawaysArray.push(giveawayData);
+        // Save the updated array
+        db.set("giveaways", newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+ 
+    // This function is called when a giveaway needs to be deleted from the database.
+    async deleteGiveaway(messageID){
+        // Remove the giveaway from the array
+        const newGiveawaysArray = db.get("giveaways").filter((giveaway) => giveaway.messageID !== messageID);
+        // Save the updated array
+        db.set("giveaways", newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+ 
+};
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
@@ -46,7 +90,7 @@ bot.on("message", async message => {
 require("./util/eventHandler")(bot);
 
 const manager = new GiveawaysManager(bot, {
-  storage: "./storage.json",
+  storage: false,
   updateCountdownEvery: 10000,
   default: {
     botsCanWin: false,
