0238a52c34837deb18e93a3cdd24269c77ba89d7
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-12 22:06:07 -0400

    update help command

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-    let cmd = bot.commands.map(x => `**g!${x.config.name}** **-** ${x.config.description}`)
+    let cmd = bot.commands.map(x => `**${x.config.usage}** **-** ${x.config.description}`)
 
     message.channel.send(`🎉**GiveawayBot Commands**\n\n${cmd.join("\n")}\n\nTo see how this bot was created, check out this **GitHub** repository: **https://github.com/gavserve/GiveawayBot**`)
 }
